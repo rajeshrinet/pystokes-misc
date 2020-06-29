1a4c4ba6dc2831a28b50316682ba230e9d9216f2
Rajesh Singh <rajeshrinet@gmail.com>
2020-06-29 18:50:03 +0100

    minor

--- pystokes/__init__.py
+++
@@ -1,9 +0,0 @@
-import pystokes.interface
-import pystokes.periodic
-import pystokes.twoWalls
-import pystokes.unbounded
-import pystokes.utils
-import pystokes.wallBounded
-import pystokes.forceFields
-import pystokes.phoreticUnbounded
-import pystokes.phoreticWallBounded
